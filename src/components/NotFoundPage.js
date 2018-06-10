1a1de87bcb25660057f7a1a53d76e910554f37cd
Georgios Lymperopoulos <glympe77@gmail.com>
2018-06-10 17:21:05 +0300

    added favicon, better 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const NotFoundPage = () => (
-    <div>
-      <h1>This is 404 page</h1>
+  <div className='not-found '>
+    <div className='not-found--overlay' />
+    <div className='not-found--text'>
+      <span>404.</span>
+      <br />
+      <span>This page does not exist.</span>
     </div>
+  </div>
   );
 
 export default NotFoundPage;
